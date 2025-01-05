1f9f82663bd2b025726bbe6beebce8b69e9a871e
Marc <marc@marc>
2025-01-05 15:50:31 -0500

    more

--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -5,10 +5,10 @@ then
     echo "Environment is Blue. Switching to Green"
     kubectl set image deployment backend-green backend=mzucc/tracker-backend:ci-build-$BUILD_NUM
     kubectl rollout status deployment backend-green
-    kubectl patch ingress backend --patch deployment/k8s/backend-ingress-patch-green.yaml
+    kubectl patch ingress backend --type merge --patch-file deployment/k8s/backend-ingress-patch-green.yaml
 else
     echo "Environment is Green. Switching to Blue"
     kubectl set image deployment backend-green backend=mzucc/tracker-backend:ci-build-$BUILD_NUM
     kubectl rollout status deployment backend-blue
-    kubectl patch ingress backend --patch deployment/k8s/backend-ingress-patch-blue.yaml
+    kubectl patch ingress backend --type merge --patch-file deployment/k8s/backend-ingress-patch-blue.yaml
 fi
